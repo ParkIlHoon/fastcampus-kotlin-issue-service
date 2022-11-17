b4e94c4ec94bddc515d93725750b6c7613870296
박일훈 <chiwoo2074@gmail.com>
2022-11-17 21:49:51 +0900

    이슈 목록 조회 API 개발

--- issue-service/src/main/kotlin/com/fastcampus/issueservice/domain/IssueRepository.kt
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/domain/IssueRepository.kt
@@ -1,9 +1,13 @@
 package com.fastcampus.issueservice.domain
 
+import com.fastcampus.issueservice.domain.enums.IssueStatus
 import org.springframework.data.jpa.repository.JpaRepository
 
 /**
  * 이슈 리포지토리
  */
 interface IssueRepository: JpaRepository<Issue, Long> {
+
+    fun findAllByUserIdAndStatusOrderByCreatedAtDesc(userId: Long, status: IssueStatus): List<Issue>
+
 }
