e55bc97bd09768ccb43b08c4267e25d449b44320
박일훈 <chiwoo2074@gmail.com>
2023-03-13 16:00:58 +0900

    사용자 서비스 프로젝트 init

---
+++ user-service/build.gradle.kts
@@ -0,0 +1,22 @@
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("at.favre.lib:bcrypt:0.9.0")
+
+    runtimeOnly("io.r2dbc:r2dbc-h2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
